c98ba7892d09c9930ed39be9f828280ce4bd8f01
rtv313isc <rtv313.isc@hotmail.com>
2020-03-19 15:05:31 -0600

    4_5 Done

--- Chapter4/4_5IsBST.py
+++ Chapter4/4_5IsBST.py
@@ -1,52 +1,49 @@
-from Helper import Node,create_tree
+from Helper import Node, create_tree
 
-def collect_nodes(root_node,nodes_set):
 
+def collect_nodes(root_node, nodes_set):
     if root_node == None:
         return
     nodes_set.add(root_node.data)
-    collect_nodes(root_node.left_son,nodes_set)
-    collect_nodes(root_node.right_son,nodes_set)
+    collect_nodes(root_node.left_son, nodes_set)
+    collect_nodes(root_node.right_son, nodes_set)
     return
+
 
 def get_min(root_node):
     nodes_set = set()
-    collect_nodes(root_node,nodes_set)
+    collect_nodes(root_node, nodes_set)
     return min(list(nodes_set))
+
 
 def get_max(root_node):
     nodes_set = set()
     collect_nodes(root_node, nodes_set)
     return max(list(nodes_set))
 
-def check_bst(root_node):
 
-    if root_node == None:
+def check_bst(root_node):
+    if root_node.left_son == None and root_node.right_son == None:
         return True
 
-    left_value = None
-    if root_node.left_son != None:
-        left_value = get_max(root_node.left_son)
+    if root_node.left_son == None and root_node.right_son != None and root_node.data <= get_min(root_node.right_son):
+        return True and check_bst(root_node.right_son)
 
-    right_value = None
-    if root_node.right_son != None:
-        right_value = get_min(root_node.right_son)
+    if root_node.left_son != None and root_node.right_son == None and root_node.data >= get_min(root_node.right_son):
+        return True and check_bst(root_node.left_son)
 
-    value_general = False
+    max_value_left = get_max(root_node.left_son)
+    min_value_right = get_min(root_node.right_son)
+
+    if max_value_left <= root_node.data and root_node.data <= min_value_right:
+        return True and check_bst(root_node.left_son) and check_bst(root_node.right_son)
+
+    return False
 
 
-
-    if left_value <= root_node.data and root_node.data <= right_value:
-        value_general = True
-        if check_bst(root_node.left_son) and check_bst(root_node.right_son) and value_general:
-            return True
-        else:
-            return False
-
-    return value_general
-
-sorted_array = [0,1,2,3,4,5,6,7]
-root_node = create_tree(sorted_array,0,len(sorted_array)-1)
-
+sorted_array = [0, 1, 2, 3, 4, 5, 6, 7]
+root_node = create_tree(sorted_array, 0, len(sorted_array) - 1)
+sorted_array2 = [0, 1, 8, 3, 4, 5, 6, 7]
+root_node2 = create_tree(sorted_array2, 0, len(sorted_array2) - 1)
 print(check_bst(root_node))
-
+print(check_bst(root_node2))
