3caba38e278bd72da48eb9afef8dbe33af0a73f5
rtv313isc <rtv313.isc@hotmail.com>
2020-04-18 14:50:44 -0500

    Fix bug 2_1

--- Chapter2/2_1RemoveDuplicates.py
+++ Chapter2/2_1RemoveDuplicates.py
@@ -2,26 +2,32 @@ from LinkedList import LinkedList
 
 link_list = LinkedList()
 link_list.add_node(1)
+link_list.add_node(1)
 link_list.add_node(2)
 link_list.add_node(3)
 link_list.add_node(4)
 link_list.add_node(1)
 link_list.add_node(2)
 link_list.add_node(3)
-
+link_list.add_node(3)
 
 def remove_duplicates(linked_list):
     current = link_list.head
 
     while current != None:
 
-        runner = current
+        runner = link_list.head;
 
         while runner != None:
 
-            if runner.next != None:
-                if current.data == runner.next.data:
-                    runner.next = runner.next.next
+            if runner != current:
+                if runner.data == current.data:
+                    if runner.next != None:
+                        runner.data = runner.next.data
+                        runner.next = runner.next.next;
+                    else:
+                        runner.next = None
+
 
             runner = runner.next
 
